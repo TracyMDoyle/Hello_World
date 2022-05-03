0aeecfc77153f19750a32c0e616e27e8d97b2745
Tracy Doyle <doyletracy@hotmail.com>
2022-05-02 20:15:09 -0400

    fixed error on line 10

--- hello_world.py
+++ hello_world.py
@@ -1,7 +1,4 @@
 # 1. TASK: print "Hello World"
-from types import TracebackType
-
-
 print('Hello World')
 # 2. print "Hello Noelle!" with the name in a variable
 name = "Tracy"
@@ -10,7 +7,7 @@ print('Hello ' + name)	# with a +
 # 3. print "Hello 42!" with the number in a variable
 name = 42
 print('Hello', name)	# with a comma
-print('Hello ' + str(name))	# with a +	-- this one should give us an error!
+print('Hello ' + str(name))	# with a +	-- this one should give us an error!, nope I fixed it!
 # 4. print "I love to eat sushi and pizza." with the foods in variables
 fave_food1 = "sushi"
 fave_food2 = "pizza"
